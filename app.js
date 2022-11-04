b74c0f02013b0c9d9cd854cc9c9105952738457c
case02 <cstocker0212@gmail.com>
2022-11-03 17:12:32 -0700

    good save spot

---
+++ app.js
@@ -0,0 +1,19 @@
+// import LocomotiveScroll from 'locomotive-scroll';
+
+// const scroll = new LocomotiveScroll({
+// 	el: document.querySelector('[data-scroll-container]'),
+// 	smooth: true,
+//  multiplier: 10,
+// });
+
+const aboutMesection = document.querySelector('#about')
+const aboutLink = document.querySelector('.navbar__item--about')
+const worksLink = document.querySelector('.navbar__item--works')
+const contactLink = document.querySelector('.navbar__item--contact')
+
+// document
+// 	.getElementsById('about')
+// 	.addEventListener('scroll', () => {
+// 		const scrolled = document.getElementsById('about').scrollTop;
+// 		console.log(scrolled);
+// 	});
